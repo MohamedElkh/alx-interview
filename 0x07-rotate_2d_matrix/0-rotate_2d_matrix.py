a86166978d6c72fba94f4ed95c97dec6a06f2416
mohamed <mohamedkharashy27@yahoo.com>
2024-02-19 00:40:29 -0800

    1

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""this func to define  rotates an nxn 2D matrix 90 degree"""
+
+
+def rotate_2d_matrix(matrix):
+    """this func to rotate 2d square matrix 90 degrees
+    args:
+        matrix: square matrix
+    """
+    n = len(matrix)
+
+    for x in range(n):
+        for k in range(x):
+            tmp = matrix[x][k]
+
+            matrix[x][k] = matrix[k][x]
+            matrix[k][x] = tmp
+
+    for x in range(n):
+        for k in range(int(n / 2)):
+            tmp = matrix[x][k]
+            matrix[x][k] = matrix[x][n-1-k]
+            matrix[x][n-1-k] = tmp
